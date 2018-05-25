c406ce25f408930143b996591d3e32f9c4f5c99c
deka <deka@chapi177>
2018-05-25 16:03:19 -0500

    sale linux 3

--- src/main/java/com/unia/controller/ItemBean.java
+++ src/main/java/com/unia/controller/ItemBean.java
@@ -14,7 +14,7 @@ import org.omnifaces.util.Faces;
 import com.unia.model.ItemTipoTramite;
 import com.unia.service.IItemTipoTramiteService;
 
-//llego a linux y sale
+//llego a linux y sale 3
 
 @Named
 @ViewScoped
